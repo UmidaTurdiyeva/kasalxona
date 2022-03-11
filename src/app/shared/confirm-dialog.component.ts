6a95337313a17c09bcbc817292a111afa8ab337b
Umida Turdiyeva <umidabunyod@bk.ru>
2022-03-11 22:03:15 +0500

    confirm oynasi qilindi

--- src/app/shared/confirm-dialog.component.ts
+++ src/app/shared/confirm-dialog.component.ts
@@ -1,24 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 
 @Component({
 
 
     template: `
-    <h1></h1>
+    <h1 matDialogTitle>{{title}}</h1>
+    <mat-dialog-content>
+        {{msg}}
+    </mat-dialog-content>
+
+    <mat-dialog-actions>
+        <button mat-raised-button color="accent" matDialogClose>Bekor qilish</button>
+        <button mat-raised-button color="primary" (click)="ok()">Tasdiqlash</button>
+    </mat-dialog-actions>
+
     `
 
-   
+
 })
 
 export class ConfirmDialogComponent {
-    title = '',
-    msg = ''
+    title = '';
+    msg = '';
 
-    constructor() {
+    constructor(
+        public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: {title: string, mgs: string},
+    ) {
+        this.title = data.title;
+        this.msg = data.mgs;    
 
-        openDialog(){
-
-        }
+      }
+      ok(){
+          this.dialogRef.close(true);
+      }
     }
-}
